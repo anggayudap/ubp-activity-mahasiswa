bc57c026af4d3525192638383a7c2deb103cb6ab
Angga Yuda <anggayudap@outlook.com>
2022-08-30 23:12:57 +0700

    migrate master layout main menu done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -29,120 +29,12 @@
 
 <!-- BEGIN: Body-->
 
-<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
-      data-menu="vertical-menu-modern" data-col="">
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
 @include('sweetalert::alert')
 
 <!-- BEGIN: Header-->
 @include('layouts.navbar')
-<ul class="main-search-list-defaultlist d-none">
-    <li class="d-flex align-items-center"><a href="#">
-            <h6 class="section-label mt-75 mb-0">Files</h6>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                                   href="app-file-manager.html">
-            <div class="d-flex">
-                <div class="me-75"><img src="{{ URL::asset('vuexy/images/icons/xls.png') }}" alt="png" height="32">
-                </div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">Two new item submitted</p><small class="text-muted">Marketing
-                        Manager</small>
-                </div>
-            </div>
-            <small class="search-data-size me-50 text-muted">&apos;17kb</small>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                                   href="app-file-manager.html">
-            <div class="d-flex">
-                <div class="me-75"><img src="{{ URL::asset('vuexy/images/icons/jpg.png') }}" alt="png" height="32">
-                </div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">52 JPG file Generated</p><small class="text-muted">FontEnd
-                        Developer</small>
-                </div>
-            </div>
-            <small class="search-data-size me-50 text-muted">&apos;11kb</small>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                                   href="app-file-manager.html">
-            <div class="d-flex">
-                <div class="me-75"><img src="{{ URL::asset('vuexy/images/icons/pdf.png') }}" alt="png" height="32">
-                </div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">25 PDF File Uploaded</p><small class="text-muted">Digital
-                        Marketing Manager</small>
-                </div>
-            </div>
-            <small class="search-data-size me-50 text-muted">&apos;150kb</small>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                                   href="app-file-manager.html">
-            <div class="d-flex">
-                <div class="me-75"><img src="{{ URL::asset('vuexy/images/icons/doc.png') }}" alt="png" height="32">
-                </div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">Anna_Strong.doc</p><small class="text-muted">Web Designer</small>
-                </div>
-            </div>
-            <small class="search-data-size me-50 text-muted">&apos;256kb</small>
-        </a></li>
-    <li class="d-flex align-items-center"><a href="#">
-            <h6 class="section-label mt-75 mb-0">Members</h6>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                                   href="app-user-view-account.html">
-            <div class="d-flex align-items-center">
-                <div class="avatar me-75"><img src="{{ URL::asset('vuexy/images/portrait/small/avatar-s-8.jpg') }}"
-                                               alt="png"
-                                               height="32"></div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">John Doe</p><small class="text-muted">UI designer</small>
-                </div>
-            </div>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                                   href="app-user-view-account.html">
-            <div class="d-flex align-items-center">
-                <div class="avatar me-75"><img src="{{ URL::asset('vuexy/images/portrait/small/avatar-s-1.jpg') }}"
-                                               alt="png"
-                                               height="32"></div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">Michal Clark</p><small class="text-muted">FontEnd
-                        Developer</small>
-                </div>
-            </div>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                                   href="app-user-view-account.html">
-            <div class="d-flex align-items-center">
-                <div class="avatar me-75"><img src="{{ URL::asset('vuexy/images/portrait/small/avatar-s-14.jpg') }}"
-                                               alt="png"
-                                               height="32"></div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">Milena Gibson</p><small class="text-muted">Digital Marketing
-                        Manager</small>
-                </div>
-            </div>
-        </a></li>
-    <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                                   href="app-user-view-account.html">
-            <div class="d-flex align-items-center">
-                <div class="avatar me-75"><img src="{{ URL::asset('vuexy/images/portrait/small/avatar-s-6.jpg') }}"
-                                               alt="png"
-                                               height="32"></div>
-                <div class="search-data">
-                    <p class="search-data-title mb-0">Anna Strong</p><small class="text-muted">Web Designer</small>
-                </div>
-            </div>
-        </a></li>
-</ul>
-<ul class="main-search-list-defaultlist-other-list d-none">
-    <li class="auto-suggestion justify-content-between"><a
-                class="d-flex align-items-center justify-content-between w-100 py-50">
-            <div class="d-flex justify-content-start"><span class="me-75" data-feather="alert-circle"></span><span>No results found.</span>
-            </div>
-        </a></li>
-</ul>
+
 <!-- END: Header-->
 
 
@@ -152,16 +44,42 @@
 
 <!-- BEGIN: Content-->
 <div class="app-content content ">
-    <div class="content-overlay"></div>
-    <div class="header-navbar-shadow"></div>
-    <div class="content-wrapper container-xxl p-0">
-        <div class="content-header row">
-        </div>
-        <div class="content-body">
-            @yield('content')
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            <div class="content-header row">
+                <div class="content-header-left col-md-9 col-12 mb-2">
+                    <div class="row breadcrumbs-top">
+                        <div class="col-12">
+                            <h2 class="content-header-title float-left mb-0">Layout Empty</h2>
+                            <div class="breadcrumb-wrapper">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Home</a>
+                                    </li>
+                                    <li class="breadcrumb-item"><a href="#">Layouts</a>
+                                    </li>
+                                    <li class="breadcrumb-item active">Layout Empty
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-header-right text-md-right col-md-3 col-12 d-md-block d-none">
+                    <div class="form-group breadcrumb-right">
+                        <div class="dropdown">
+                            <button class="btn-icon btn btn-primary btn-round btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="grid"></i></button>
+                            <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="app-todo.html"><i class="mr-1" data-feather="check-square"></i><span class="align-middle">Todo</span></a><a class="dropdown-item" href="app-chat.html"><i class="mr-1" data-feather="message-square"></i><span class="align-middle">Chat</span></a><a class="dropdown-item" href="app-email.html"><i class="mr-1" data-feather="mail"></i><span class="align-middle">Email</span></a><a class="dropdown-item" href="app-calendar.html"><i class="mr-1" data-feather="calendar"></i><span class="align-middle">Calendar</span></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                @yield('content')
+
+            </div>
         </div>
     </div>
-</div>
 <!-- END: Content-->
 
 <div class="sidenav-overlay"></div>
@@ -169,13 +87,10 @@
 
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2021<a
-                    class="ms-25" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a><span
-                    class="d-none d-sm-inline-block">, All rights Reserved</span></span><span
-                class="float-md-end d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
-</footer>
-<button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
-<!-- END: Footer-->
+        <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2021<a class="ml-25" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
+    </footer>
+    <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
+    <!-- END: Footer-->
 
 
 <script src="{{ URL::asset('js/hasil_combine.js') }}"></script>
