a7ede6a627f9d547966c0ba3235605f0aa28a730
Angga Yuda <anggayudap@outlook.com>
2022-09-02 17:35:22 +0700

    update model

--- app/Models/Kegiatan.php
+++ app/Models/Kegiatan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kegiatan extends Model {
     use HasFactory;
-    
+
     protected $table = 'kegiatans';
 
     protected $guarded = [
@@ -15,7 +15,22 @@ class Kegiatan extends Model {
     ];
 
     protected $fillable = [
-
+        'nim',
+        'nama_mahasiswa',
+        'periode_id',
+        'tahun_periode',
+        'nama_kegiatan',
+        'tanggal_mulai',
+        'tanggal_akhir',
+        'klasifikasi_id',
+        'surat_tugas',
+        'foto_kegiatan',
+        'url_event',
+        'bukti_sertifikat',
+        'keterangan',
+        'checked_kemahasiswaan',
+        'checked_warek',
+        'decision_warek',
         'created_at',
         'updated_at',
     ];
--- app/Models/Periode.php
+++ app/Models/Periode.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Periode extends Model
-{
+class Periode extends Model {
     use HasFactory;
+
+    protected $table = 'periodes';
+
+    protected $guarded = [
+        'id',
+    ];
+
+    protected $fillable = [
+        'periode_awal',
+        'periode_akhir',
+        'created_at',
+        'updated_at',
+    ];
 }
