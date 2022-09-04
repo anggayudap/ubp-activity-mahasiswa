5c357b0d647a61f5d45a33e0806cd9e91bfcf4a8
Angga Yuda <anggayudap@outlook.com>
2022-09-04 15:01:51 +0700

    master prodi done

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -2,83 +2,110 @@
 
 namespace App\Http\Controllers;
 
+use DataTables;
+use App\Models\Prodi;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ProdiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function index(Request $request) {
+        if ($request->ajax()) {
+            $data = Prodi::select(['id', 'kode_prodi', 'nama_prodi']);
+            return Datatables::of($data)->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<div class="dropdown">
+                        <a class="btn btn-sm btn-icon px-0" data-toggle="dropdown" aria-expanded="false"><i data-feather="more-vertical"></i></a>
+                        <div class="dropdown-menu dropdown-menu-right" style="">
+                        <a href="' . route("master.prodi.edit", $row->id) . '" class="dropdown-item"><i data-feather="edit"></i> Edit</a>
+                        <form action="' . route("master.prodi.destroy", [$row->id]) . '" method="POST" id="form-delete-' . $row->id . '" style="display: inline">
+                        ' . csrf_field() . '
+                        ' . method_field("DELETE") . '
+                        <a href="#" onclick="submit_delete(' . $row->id . ')" class="dropdown-item"><i data-feather="trash-2"></i> Delete</a>
+                        </form>
+                        </div>
+                        </div>';
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->removeColumn('id')
+                ->make(true);
+        }
+
+        return view('master.prodi.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function create() {
+        return view('master.prodi.form');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'kode_prodi' => 'required',
+            'nama_prodi' => 'required',
+        ]);
+
+        $post = Prodi::create([
+            'kode_prodi' => $request->kode_prodi,
+            'nama_prodi' => $request->nama_prodi,
+        ]);
+
+        if ($post) {
+            Alert::success('Berhasil!', 'Data prodi berhasil dibuat!');
+            return redirect(route('master.prodi.index'));
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again',
+                ]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function edit($id) {
+        $data = Prodi::findOrFail($id);
+
+        return view('master.prodi.form', compact('data'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+    public function update(Request $request, $id) {
+        $this->validate($request, [
+            'kode_prodi' => 'required',
+            'nama_prodi' => 'required',
+        ]);
+
+        $site = Prodi::findOrFail($id);
+
+        $site->update([
+            'kode_prodi' => $request->kode_prodi,
+            'nama_prodi' => $request->nama_prodi,
+        ]);
+
+        if ($site) {
+            Alert::success('Berhasil!', 'Data prodi berhasil diupdate!');
+            return redirect(route('master.prodi.index'));
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again',
+                ]);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+    public function destroy($id) {
+        $prodi = Prodi::findOrFail($id);
+        $prodi->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if ($prodi) {
+            Alert::success('Berhasil!', 'Data prodi berhasil dihapus!');
+            return redirect(route('master.prodi.index'));
+        } else {
+            Alert::error('Gagal!', 'Data prodi tidak dapat dihapus!');
+            return redirect(route('master.prodi.index'));
+        }
     }
 }
--- resources/views/master/prodi/form.blade.php
+++ resources/views/master/prodi/form.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
-@section('title', 'Form Tambah Data Departemen')
-@section('menu-title', 'Master Data Departemen')
+@section('title', 'Form Data Prodi')
+@section('menu-title', 'Master Data Prodi')
 
 
 @section('content')
@@ -9,58 +9,47 @@
         <div class="col-md-12 col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">{{ isset($data) ? 'Edit' : 'Tambah' }} Data Departemen</h4>
+                    <h4 class="card-title">{{ isset($data) ? 'Edit' : 'Tambah' }} Data Prodi</h4>
                 </div>
                 <div class="card-body">
-                    <form action="{{ isset($data) ? route('master.departement.update', $data->id) : route('master.departement.store') }}"
+                    <form action="{{ isset($data) ? route('master.prodi.update', $data->id) : route('master.prodi.store') }}"
                         method="post" class="form form-horizontal need-validation" novalidate>
                         @csrf
                         @if (isset($data))
                             @method('PUT')
-                            @endif
-                            
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="form-group row">
-                                        <div class="col-sm-3 col-form-label">
-                                            <label for="name">Nama Departemen</label>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <input type="text" id="name" class="form-control" name="name"
-                                                placeholder="Nama Departemen" value="{{ isset($data) ? $data->name : '' }}">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-group row">
-                                        <div class="col-sm-3 col-form-label">
-                                            <label for="code">Kode</label>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <input type="text" id="code" class="form-control" name="code"
-                                                placeholder="Kode" {{ isset($data) ? 'readonly' : '' }} value="{{ isset($data) ? $data->code : '' }}">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-group row">
-                                        <div class="col-sm-3 col-form-label">
-                                            <label for="cost-center">Cost Center</label>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <input type="text" id="cost-center" class="form-control" name="cost_center"
-                                                placeholder="Cost Center" value="{{ isset($data) ? $data->cost_center : '' }}">
-                                        </div>
-                                    </div>
-                                </div>
+                        @endif
 
-                                <div class="col-sm-9 offset-sm-3">
-                                    <button type="submit"
-                                        class="btn btn-primary mr-1 waves-effect waves-float waves-light">Submit
-                                    </button>
-                                    <button type="reset" class="btn btn-outline-secondary waves-effect">Reset</button>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group row">
+                                    <div class="col-sm-3 col-form-label">
+                                        <label for="name">Kode Prodi</label>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <input type="text" id="kode-prodi" class="form-control" name="kode_prodi"
+                                            placeholder="Kode Prodi" value="{{ isset($data) ? $data->kode_prodi : '' }}">
+                                    </div>
                                 </div>
                             </div>
+                            <div class="col-12">
+                                <div class="form-group row">
+                                    <div class="col-sm-3 col-form-label">
+                                        <label for="code">Nama Prodi</label>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <input type="text" id="nama-prodi" class="form-control" name="nama_prodi"
+                                            placeholder="Nama Prodi" value="{{ isset($data) ? $data->nama_prodi : '' }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-9 offset-sm-3">
+                                <button type="submit"
+                                    class="btn btn-primary mr-1 waves-effect waves-float waves-light">Submit
+                                </button>
+                                <button type="reset" class="btn btn-outline-secondary waves-effect">Reset</button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
