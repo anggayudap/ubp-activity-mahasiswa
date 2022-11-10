51fd71ef62d98fb2ac578940344ba496c22641b5
Angga Yuda <anggayudap@outlook.com>
2022-11-10 15:50:47 +0700

    api done

--- app/Http/Controllers/API/KegiatanController.php
+++ app/Http/Controllers/API/KegiatanController.php
@@ -2,19 +2,78 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\Kegiatan;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\KegiatanResource;
+use App\Models\Kegiatan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
 
-class KegiatanController extends Controller
-{
-    public function index()
-    {
-        //get posts
-        $posts = Kegiatan::latest()->paginate(5);
+class KegiatanController extends Controller {
+    public function all(Request $request) {
+        $posts = Kegiatan::select('id', 'klasifikasi_id', 'nama_kegiatan', 'nim', 'bukti_kegiatan')
+            ->where('approval', 'approve')
+            ->with(['klasifikasi'])
+            ->get();
 
-        //return collection of posts as a resource
-        return new KegiatanResource('200', 'List Data Kegiatan', $posts);
+        $output = array();
+
+        if ($posts->count() > 0) {
+            foreach ($posts as $post) {
+                $output[] = [
+                    'nama-kegiatan' => $post->nama_kegiatan,
+                    'nim' => $post->nim,
+                    'id-kegiatan' => $post->id,
+                    'klasifikasi' => $post->klasifikasi->name_kegiatan,
+                    'bukti-sertifikat' => URL::asset($post->bukti_kegiatan),
+                ];
+            }
+
+            //return collection of posts as a resource
+            return new KegiatanResource('200', 'Data Kegiatan : Ditemukan', $output);
+           
+        }
+
+        return new KegiatanResource('404', 'Data Kegiatan : Tidak Ditemukan', $output);
+    }
+
+    public function filtered(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'nim' => 'required',
+        ],
+            [
+                'nim.required' => 'Masukkan NIM Mahasiswa !',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return new KegiatanResource('401', 'Silahkan Isi Bidang Yang Kosong', $validator->errors());
+
+        } else {
+            $posts = Kegiatan::select('id', 'klasifikasi_id', 'nama_kegiatan', 'nim', 'bukti_kegiatan')
+                ->where('approval', 'approve')
+                ->where('nim', $request->input('nim'))
+                ->with(['klasifikasi'])
+                ->get();
+
+            $output = array();
+
+            if ($posts->count() > 0) {
+                foreach ($posts as $post) {
+                    $output[] = [
+                        'nama-kegiatan' => $post->nama_kegiatan,
+                        'nim' => $post->nim,
+                        'id-kegiatan' => $post->id,
+                        'klasifikasi' => $post->klasifikasi->name_kegiatan,
+                        'bukti-sertifikat' => URL::asset($post->bukti_kegiatan),
+                    ];
+                }
+
+                //return collection of posts as a resource
+                return new KegiatanResource('200', 'List Data Kegiatan : Ditemukan', $output);
+            }
+
+            return new KegiatanResource('404', 'List Data Kegiatan : Tidak Ditemukan', $output);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::apiResource('/kegiatan_mahasiswa', KegiatanController::class);
+Route::get('kegiatan_mahasiswa/all', [KegiatanController::class, 'all']);
+Route::post('kegiatan_mahasiswa/filter', [KegiatanController::class, 'filtered']);
