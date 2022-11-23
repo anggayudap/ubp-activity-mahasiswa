14d5422179605e8c7fad439bc2ff0bbd7df0afeb
Angga Yuda <anggayudap@outlook.com>
2022-11-23 15:44:54 +0700

    update favicon

--- resources/views/layouts/master_table.blade.php
+++ resources/views/layouts/master_table.blade.php
@@ -9,8 +9,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
     
     <title>@yield('title') - Sistem Pengajuan Proposal dan Kegiatan Mahasiswa UBP Karawang</title>
-    <link rel="apple-touch-icon" href="{{ asset('vuexy') }}/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('vuexy') }}/images/ico/favicon.ico">
+    <link rel="icon" type="image/png" href="{{ asset('logo-kecil.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
         rel="stylesheet">
 
