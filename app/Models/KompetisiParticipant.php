4533eea2ceb9d6857d11bfe4587914f781305515
DESKTOP-0AS8R4V\user <anggayudap@outlook.com>
2023-01-14 08:39:25 +0700

    update model

--- app/Models/KompetisiParticipant.php
+++ app/Models/KompetisiParticipant.php
@@ -3,33 +3,57 @@
 namespace App\Models;
 
 use App\Models\Kompetisi;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\KompetisiParticipantMember;
 use App\Models\KompetisiParticipantReview;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class KompetisiParticipant extends Model
-{
+class KompetisiParticipant extends Model {
     use HasFactory;
 
     protected $table = 'kompetisi_participants';
 
     protected $guarded = ['id'];
 
-    protected $fillable = ['kompetisi_id', 'nip_dosen_pembimbing', 'nama_dosen_pembimbing', 'email_dosen_pembimbing', 'prodi_dosen_pembimbing', 'judul', 'tahun', 'nama_skema', 'deskripsi_skema', 'file_upload', 'review', 'catatan', 'tanggal_approval', 'user_approval', 'nama_approval', 'keputusan', 'is_editable', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'kompetisi_id',
+        'id_dosen_pembimbing',
+        'nip_dosen_pembimbing',
+        'nama_dosen_pembimbing',
+        'email_dosen_pembimbing',
+        'prodi_dosen_pembimbing',
+        'judul',
+        'tahun',
+        'nama_skema',
+        'deskripsi_skema',
+        'file_upload',
+        'id_dosen_penilai',
+        'nip_dosen_penilai',
+        'nama_dosen_penilai',
+        'email_dosen_penilai',
+        'prodi_dosen_penilai',
+        'review',
+        'catatan',
+        'tanggal_approval',
+        'user_approval',
+        'nama_approval',
+        'keputusan',
+        'is_editable',
+        'user_created',
+        'user_updated',
+        'created_at',
+        'updated_at',
+    ];
 
-    public function kompetisi()
-    {
+    public function kompetisi() {
         return $this->belongsTo(Kompetisi::class, 'kompetisi_id');
     }
 
-    public function member()
-    {
+    public function member() {
         return $this->hasMany(KompetisiParticipantMember::class, 'participant_id');
     }
 
-    public function review()
-    {
+    public function review() {
         return $this->hasMany(KompetisiParticipantReview::class, 'paricipant_id');
     }
 }
