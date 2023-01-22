97293ac791eb75f180a0708a91965a9130f372f1
DESKTOP-0AS8R4V\user <anggayudap@outlook.com>
2023-01-22 21:38:35 +0700

    dashboard mahasiswa : kompetisi done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Kegiatan;
 use App\Models\KompetisiParticipant;
 use App\Models\Proposal;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,7 +18,15 @@ class DashboardController extends Controller
 
         if ($user->hasRole('mahasiswa')) {
             $result['proposal'] = Proposal::where('nim', session('user.id'))->get();
+
             $result['kegiatan'] = Kegiatan::where('nim', session('user.id'))->get();
+
+            $result['kompetisi'] = KompetisiParticipant::select(['id', 'kompetisi_id', 'created_at', 'nama_skema', 'nama_dosen_pembimbing', 'status', 'is_editable'])
+            ->with(['member'])
+            ->whereHas('member', function (Builder $query) {
+                $query->where('nim', session('user.id'));
+            })->get();
+            
         } else {
             if ($user->hasRole('dosen')) {
                 $result['kompetisi_review_dosen'] = KompetisiParticipant::where('status', 'in_review')
--- resources/views/dashboard/dashboard_mahasiswa.blade.php
+++ resources/views/dashboard/dashboard_mahasiswa.blade.php
@@ -215,8 +215,88 @@
                 </div>
             </div>
 
+            {{-- KOMPETISI --}}
+            <div class="row pb-3">
+                <div class="col-12">
+                    <h3 class="card-title">Kompetisi</h3>
+                </div>
+                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-2">
+                    <div class="media">
+                        <a href="{{ route('kompetisi.history') }}" class="avatar bg-light-warning mr-2">
+                            <div class="avatar-content">
+                                <i data-feather="user-check" class="font-medium-5"></i>
+                            </div>
+                        </a>
+                        <div class="media-body my-auto">
+                            @php
+                                $filtered = $result['kompetisi']->filter(function ($value, $key) {
+                                    return $value->status == 'pending';
+                                });
+                            @endphp
+                            <h4 class="font-weight-bolder mb-0">{{ $filtered->count() }}</h4>
+                            <p class="card-text font-small-3 mb-0">Pending ACC Kemahasiswaan</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-2">
+                    <div class="media">
+                        <a href="{{ route('kompetisi.history') }}" class="avatar bg-light-danger mr-2">
+                            <div class="avatar-content">
+                                <i data-feather="user-x" class="font-medium-5"></i>
+                            </div>
+                        </a>
+                        <div class="media-body my-auto">
+                            @php
+                                $filtered = $result['kompetisi']->filter(function ($value, $key) {
+                                    return $value->status == 'reject';
+                                });
+                            @endphp
+                            <h4 class="font-weight-bolder mb-0">{{ $filtered->count() }}</h4>
+                            <p class="card-text font-small-3 mb-0">Reject ACC Kemahasiswaan</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-2">
+                    <div class="media">
+                        <a href="{{ route('kompetisi.history') }}" class="avatar bg-light-info mr-2">
+                            <div class="avatar-content">
+                                <i data-feather="edit" class="font-medium-5"></i>
+                            </div>
+                        </a>
+                        <div class="media-body my-auto">
+                            @php
+                                $filtered = $result['kompetisi']->filter(function ($value, $key) {
+                                    return $value->status == 'in_review';
+                                });
+                            @endphp
+                            <h4 class="font-weight-bolder mb-0">{{ $filtered->count() }}</h4>
+                            <p class="card-text font-small-3 mb-0">Dalam proses Review</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-2">
+                    <div class="media">
+                        <a href="{{ route('kompetisi.history') }}" class="avatar bg-light-primary mr-2">
+                            <div class="avatar-content">
+                                <i data-feather="check-circle" class="font-medium-5"></i>
+                            </div>
+                        </a>
+                        <div class="media-body my-auto">
+                            @php
+                                $filtered = $result['kompetisi']->filter(function ($value, $key) {
+                                    return $value->status == 'completed';
+                                });
+                            @endphp
+                            <h4 class="font-weight-bolder mb-0">{{ $filtered->count() }}</h4>
+                            <p class="card-text font-small-3 mb-0">Selesai Dinilai & Direview</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
